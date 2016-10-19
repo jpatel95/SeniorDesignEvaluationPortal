0d44131e977ea217707ab955d7a816ea87b2611f
Jimmy Patel <jpatel@scu.edu>
2016-10-18 18:33:19 -0700

    Changed judge auth to be a secred code, and admin should be able to add remove judges and teams.

--- js/login.js
+++ js/login.js
@@ -14,14 +14,8 @@ function buttonJudgeClick() {
 	document.getElementById("loginButtonRow").innerHTML = '<div class="col s6"><button class="btn waves-effect red darken-4" onClick="buttonJudgeClick()" id="buttonJudge">Judge</button></div><div class="col s6"><button class="btn waves-effect red lighten-3" onClick="buttonAdminClick()" id="buttonAdmin">Admin</button></div>';
     document.getElementById("loginForm").innerHTML = '<div class="row">' +
             '<div class="input-field col s12">' +
-             '<input id="email" type="email" class="validate" required>' +
-              '<label for="email" data-error="Invalid Data" data-success="right">Email</label>' +
-            '</div>' +
-          '</div>' +
-          '<div class="row">' +
-            '<div class="input-field col s12">' +
               '<input id="password" type="password" class="validate" required>' +
-              '<label for="password">Password</label>' +
+              '<label for="password">Code</label>' +
             '</div>' +
           '</div>' +
           '<div class="row center">' +
@@ -60,23 +54,25 @@ function buttonAdminClick() {
 
 function login(){
 	console.log("login clicked");
-  /*
-  Need to add input sanitization.
-  */
-
-
-  email = document.getElementById("email").value;
-  pass = document.getElementById("password").value;
-  loginImpl(email, pass);
-}
-
-function loginImpl(email, pass){
   if (firebase.auth().currentUser) {
     console.log("You were already signed in so logging out now");
     console.log(firebase.auth().currentUser.uid);
     firebase.auth().signOut();
   }
+  /*
+  Need to add input sanitization.
+  */
+  if(isAdminSelected){
+      email = document.getElementById("email").value;
+      pass = document.getElementById("password").value;
+      loginAdminImpl(email, pass);
+  } else {
+      pass = document.getElementById("password").value;
+      loginJudgeImpl(pass);
+  }
+}
 
+function loginAdminImpl(email, pass){
   firebase.auth().signInWithEmailAndPassword(email, pass).catch(function(error) {
     var errorCode = error.code;
     var errorMessage = error.message;
@@ -86,26 +82,48 @@ function loginImpl(email, pass){
 
   firebase.auth().onAuthStateChanged(user => {
     if(user) {
-      if(isAdminSelected){
-        var ref = firebase.database().ref("admins/uid");
-        ref.once("value").then(function(snapshot) {
-          console.log(snapshot.val());
-          if(firebase.auth().currentUser.uid == snapshot.val()){
-            console.log("Matched");
-            window.location.replace("adminSessions.html");
-          } else {
-            alert("Invalid credentials");
-          }
-        });
-      } else {
-        window.location.replace("judgeEvaluation.html");
-      }
+      window.location.replace("adminSessions.html");
     }
   });
 }
 
+function loginJudgeImpl(pass){
+  $( "#loginButtonRow" ).append('<div id="loader" class="preloader-wrapper active">' +
+    '<div class="spinner-layer spinner-red-only">' +
+      '<div class="circle-clipper left">' +
+        '<div class="circle"></div>' +
+      '</div><div class="gap-patch">' +
+        '<div class="circle"></div>' +
+      '</div><div class="circle-clipper right">' +
+        '<div class="circle"></div>' +
+      '</div>' +
+    '</div>' +
+  '</div>');
 
-
+  var ref = firebase.database().ref("judges");
+  ref.once("value").then(function(snapshot) {
+    console.log(snapshot.val());
+    isMatch = snapshot.forEach(function(childSnapshot) {
+        var key = childSnapshot.key;
+        var childData = childSnapshot.val();
+        console.log(key, childData);
+        if(pass == childData.code){
+          console.log("Match Found: " + pass);
+          localStorage.setItem("code", childData.code);
+          localStorage.setItem("name", childData.name);
+          return true;
+        }
+    });
+    if(isMatch==true){
+      console.log("Matched");
+      window.location.replace("judgeEvaluation.html");
+    } else {
+      alert("Invalid code");
+      console.log("did not match");
+      $("#loader" ).remove();
+    }
+  });
+}
 
 
 
