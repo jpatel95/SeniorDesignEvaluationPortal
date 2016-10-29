7892aafc735c25a7a6b0c8f37a945b411d1535a5
Ryan Nakagawa <rnakagawa@scu.edu>
2016-10-29 14:30:36 -0700

    import Shane's csv into Firebase, dropdowns replaced with radios, and judge form data placed into json object for firebase

--- js/judgeEvaluation.js
+++ js/judgeEvaluation.js
@@ -23,6 +23,7 @@ $("#submit").click(function(){
 
   var name = localStorage.getItem("name");
 
+/*with drop downs
   var scores = {
       "Department": $("#department").val(),
       "Session": $("#session").val(),
@@ -55,9 +56,42 @@ $("#submit").click(function(){
       "Considerations Addressed": $("#considerations").val(),
       "Comments": $("#textarea1").val()
 
-    };
+    };*/
 
-    //console.log(scores);
+    var scores = {
+
+      "Department": $("input[name=department]:checked").val(),
+      "Session": $("input[name=session]:checked").val(),
+
+      "Project Title": $("#projectTitle").val(),
+      "Advisor": $("#nameAdvisor").val(),
+      "first_name1": $("#first_name1").val(),
+      "last_name1": $("#last_name1").val(),
+      "first_name2": $("#first_name2").val(),
+      "last_name2": $("#last_name2").val(),
+      "first_name3": $("#first_name3").val(),
+      "last_name3": $("#last_name3").val(),
+      "first_name4": $("#first_name4").val(),
+      "last_name4": $("#last_name4").val(),
+      "first_name5": $("#first_name5").val(),
+      "last_name5": $("#last_name5").val(),
+
+      "Technical Accuracy": $("input[name=technical]:checked").val(),
+      "Creativity and Innovation": $("input[name=creativity]:checked").val(),
+      "Supporting Analytical Work": $("input[name=supporting]:checked").val(),
+      "Methodical Design Process Dem": $("input[name=methodical]:checked").val(),
+      "Addresses Project Complexity": $("input[name=addresses]:checked").val(),
+      "Completeness": $("input[name=expectation]:checked").val(),
+      "Design & Analysis of Tests": $("input[name=design]:checked").val(),
+      "Quality of Response During Q&A": $("input[name=quality]:checked").val(),
+      "Organization": $("input[name=organization]:checked").val(),
+      "Time Allotment": $("input[name=time]:checked").val(),
+      "Visual Aids": $("input[name=visual]:checked").val(),
+      "Confidence and Poise": $("input[name=confidence]:checked").val(),
+
+      "Considerations Addressed": $("#considerations").val(),
+      "Comments": $("#textarea1").val()
+    }
 
     var eval = {
       "judgename": name,
