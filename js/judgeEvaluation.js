d5c453eca5d684b7114992f25517e64d35ad2c8f
Jimmy Patel <jpatel@scu.edu>
2016-10-30 00:24:30 -0700

    Integrated Ryan's code, changed layout, fixed CSV issue.

--- js/judgeEvaluation.js
+++ js/judgeEvaluation.js
@@ -23,62 +23,9 @@ function logout(){
 }
 
 $("#submit").click(function(){
-
-  var name = localStorage.getItem("name");
-
-/*with drop downs
-  var scores = {
-      "Department": $("#department").val(),
-      "Session": $("#session").val(),
-      "Project Title": $("#projectTitle").val(),
-      "Advisor": $("#nameAdvisor").val(),
-      "first_name1": $("#first_name1").val(),
-      "last_name1": $("#last_name1").val(),
-      "first_name2": $("#first_name2").val(),
-      "last_name2": $("#last_name2").val(),
-      "first_name3": $("#first_name3").val(),
-      "last_name3": $("#last_name3").val(),
-      "first_name4": $("#first_name4").val(),
-      "last_name4": $("#last_name4").val(),
-      "first_name5": $("#first_name5").val(),
-      "last_name5": $("#last_name5").val(),
-
-      "Technical Accuracy": $("#technical").val(),
-      "Creativity and Innovation": $("#creativity").val(),
-      "Supporting Analytical Work": $("#supporting").val(),
-      "Methodical Design Process Dem": $("#methodical").val(),
-      "Addresses Project Complexity": $("#addresses").val(),
-      "Completeness": $("#expectation").val(),
-      "Design & Analysis of Tests": $("#design").val(),
-      "Quality of Response During Q&A": $("#quality").val(),
-      "Organization": $("#organization").val(),
-      "Time Allotment": $("#time").val(),
-      "Visual Aids": $("#visual").val(),
-      "Confidence and Poise": $("#confidence").val(),
-
-      "Considerations Addressed": $("#considerations").val(),
-      "Comments": $("#textarea1").val()
-
-    };*/
+  	var name = localStorage.getItem("name");
 
     var scores = {
-
-      "Department": $("input[name=department]:checked").val(),
-      "Session": $("input[name=session]:checked").val(),
-
-      "Project Title": $("#projectTitle").val(),
-      "Advisor": $("#nameAdvisor").val(),
-      "first_name1": $("#first_name1").val(),
-      "last_name1": $("#last_name1").val(),
-      "first_name2": $("#first_name2").val(),
-      "last_name2": $("#last_name2").val(),
-      "first_name3": $("#first_name3").val(),
-      "last_name3": $("#last_name3").val(),
-      "first_name4": $("#first_name4").val(),
-      "last_name4": $("#last_name4").val(),
-      "first_name5": $("#first_name5").val(),
-      "last_name5": $("#last_name5").val(),
-
       "Technical Accuracy": $("input[name=technical]:checked").val(),
       "Creativity and Innovation": $("input[name=creativity]:checked").val(),
       "Supporting Analytical Work": $("input[name=supporting]:checked").val(),
@@ -100,7 +47,12 @@ $("#submit").click(function(){
       "judgename": name,
       "score": scores
     };
+
     console.log(eval);
+
+    
+    var session =  $("#selectDepartment").val() +" "+ $("#selectSession").val();
+	var time =  $("#selectTime").val();
 
     var ref = firebase.database().ref("teams");
     ref.once("value").then(function(snapshot) {
@@ -111,11 +63,11 @@ $("#submit").click(function(){
           //put each team object into childData
           var childData = childSnapshot.val();
           console.log(childData);
-          if($("#projectTitle").val() == childData.Title){
+          if(session == childData.Session && time==childData.Time){
             console.log("Match Found at Key: " + childSnapshot.key);
-            var reference = firebase.database().ref("teams/" + childSnapshot.key + "/judgescores");
+            var reference = firebase.database().ref("teams/"+childSnapshot.key+"/judgescores");
             console.log(reference);
-            //reference.push(eval);
+            reference.push(eval);
             //returning true will stop looping through teams
             return true;
           }
